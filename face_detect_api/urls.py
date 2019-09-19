de073a81e2dbf0ee2bcd014ac06abe5b9aa515e0
bildad namawa <bildadnamawa@gmail.com>
2019-09-19 19:27:30 +0300

    Scripted auto-commit on change (2019-09-19 19:27:30) by gitwatch.sh

--- face_detect_api/urls.py
+++ face_detect_api/urls.py
@@ -14,12 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += [
-    path('catalog/', include('catalog.urls')),
+    url(r'^api/', include('apps.api.urls')),
 ]
