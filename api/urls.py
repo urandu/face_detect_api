9ac11752a03e18a305a961e4193e3c3227ae7adc
bildad namawa <bildadnamawa@gmail.com>
2019-09-19 22:03:11 +0300

    auto-commit on change (2019-09-19 22:03:11) by piladi-bot

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from api.views import Image
 
 urlpatterns = [
-    url(r'^new_image/$', Image.as_view(), name='Image'),
+    url(r'^image/$', Image.as_view(), name='Image'),
 ]
