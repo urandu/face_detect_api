e9633014d080cd24ab51ac47ad78120feec0d233
bildad namawa <bildadnamawa@gmail.com>
2019-09-19 22:26:01 +0300

    auto-commit on change (2019-09-19 22:26:01) by piladi-bot

--- api/views.py
+++ api/views.py
@@ -1,11 +1,19 @@
 # The future is now!
+import uuid
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.core.files.storage import default_storage
+import os
 
 
 class Image(APIView):
 
     def post(self, request, *args, **kwargs):
 
-            return Response({"you hit the view!!"}, status=status.HTTP_200_OK)
+        img = request.FILES['image']
+        img_extension = os.path.splitext(img.name)[-1]
+        default_storage.save(uuid.uuid4() + img_extension, request.FILES['image'])
+
+        return Response({"you hit the view!!"}, status=status.HTTP_200_OK)
