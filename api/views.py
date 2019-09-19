84a8f00d39673e3cdca450d609d5f141162d60e9
bildad namawa <bildadnamawa@gmail.com>
2019-09-19 22:56:38 +0300

    auto-commit on change (2019-09-19 22:56:38) by piladi-bot

--- api/views.py
+++ api/views.py
@@ -10,13 +10,19 @@ import os
 from django.conf import settings
 
 
+def upload_image(request):
+    img = request.FILES['image']
+    img_extension = os.path.splitext(img.name)[-1]
+    return default_storage.save(settings.MEDIA_URL + str(uuid.uuid4()) + img_extension, img)
+
+
+
 class Image(APIView):
 
     def post(self, request, *args, **kwargs):
 
-        upload_image = upload_image()
-        img = request.FILES['image']
-        img_extension = os.path.splitext(img.name)[-1]
-        default_storage.save(settings.MEDIA_URL+str(uuid.uuid4()) + img_extension, img)
+        upload_image(request=request)
 
         return Response({"success":"accepted"}, status=status.HTTP_202_ACCEPTED)
+
+
