9bf54e89062b94804872bccb67a639bb8a720770
bildad namawa <bildadnamawa@gmail.com>
2019-09-23 19:36:03 +0300

    auto-commit on change (2019-09-23 19:36:03) by piladi-bot

---
+++ api/models/image.py
@@ -0,0 +1,13 @@
+import os
+import uuid
+
+from django.db import models
+
+import settings
+
+class Image(models.Model):
+    image_id = models.CharField(max_length=100)
+    image_array = models.TextField(null= True)
+
+    name = models.CharField(max_length=100)
+    date_created = models.DateTimeField(auto_now_add=True)
