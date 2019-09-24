4fcf549f7f2f12fc974ee749ee6aaccc557064b2
bildad namawa <bildadnamawa@gmail.com>
2019-09-24 12:54:16 +0300

    auto-commit on change (2019-09-24 12:54:16) by piladi-bot

--- api/models/face.py
+++ api/models/face.py
@@ -12,7 +12,4 @@ class Face(models.Model):
     confidence = models.CharField(max_length=200)
     box = models.TextField(null= True)
     keypoints = models.TextField(null= True)
-    face_array = models.TextField(null= True)
-    embedding = models.TextField(null= True)
-    encoding = models.TextField(null= True)
     date_created = models.DateTimeField(auto_now_add=True)
