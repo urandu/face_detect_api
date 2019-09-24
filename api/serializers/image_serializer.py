d5ba5e149b9ae7353e899374112bb5634a294c7a
bildad namawa <bildadnamawa@gmail.com>
2019-09-24 16:32:39 +0300

    auto-commit on change (2019-09-24 16:32:39) by piladi-bot

--- api/serializers/image_serializer.py
+++ api/serializers/image_serializer.py
@@ -6,4 +6,4 @@ from api.models.image import Image
 class ImageSerializer(serializers.ModelSerializer):
     class Meta():
         model = Image
-        fields = ('image_id', 'callback_url', 'date_created')
+        fields = ('request_id', 'callback_url', 'date_created')
