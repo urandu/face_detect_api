3b2adcce22d55e34b0602b74473c7e5cce88443b
bildad namawa <bildadnamawa@gmail.com>
2019-09-24 12:58:57 +0300

    auto-commit on change (2019-09-24 12:58:57) by piladi-bot

--- api/tasks/image.py
+++ api/tasks/image.py
@@ -1,19 +1,16 @@
 # The future is now!
 import uuid
-
 from django.core.files.storage import default_storage
-import os
 from mtcnn.mtcnn import MTCNN
 from numpy import asarray
+from api.models.face import Face
 from ..models.image import Image as Image_object
 from PIL import Image as PImage
 from api.celery_app import app
 
-from django.conf import settings
-
 
 @app.task(bind=True, name='detect_faces')
-def detect_faces(self, *args,**kwargs):
+def detect_faces(self, *args, **kwargs):
     image_id = kwargs.get("image_id")
     image_object = Image_object.objects.get(image_id=image_id)
     filename = image_object.name
@@ -22,21 +19,23 @@ def detect_faces(self, *args,**kwargs):
     pixels = asarray(image)
 
     detector = MTCNN()
-    # detect faces in the image
 
+    # detect faces in the image
     results = detector.detect_faces(pixels)
 
-    # extract the bounding box from the faces
     detected_faces = list()
     for result in results:
 
-        # only detect faces with a confidence of 90% and above
-        if result['confidence'] > 0.90:
-            detected_faces.append({
-                "face_id":uuid.uuid4(),
-                "confidence": result['confidence'],
-                "bounding_box": result['box'],
-                "keypoints":result['keypoints']
-            })
+        # only detect faces with a confidence of 94% and above
+        if result['confidence'] > 0.94:
+            face_object = Face()
+            face_id = str(uuid.uuid4())
+            face_object.face_id = face_id
+            face_object.image_id = image_id
+            face_object.confidence = result['confidence']
+            face_object.box = result['box']
+            face_object.keypoints = result['keypoints']
+            face_object.save()
+            detected_faces.append(face_id)
 
     return detected_faces
