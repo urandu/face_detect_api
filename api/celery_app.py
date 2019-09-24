39650060b52094e52b10b6cc7017c87f6445f862
bildad namawa <bildadnamawa@gmail.com>
2019-09-24 13:31:27 +0300

    auto-commit on change (2019-09-24 13:31:27) by piladi-bot

--- api/celery_app.py
+++ api/celery_app.py
@@ -2,6 +2,6 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'face_detect_api.settings')
 app = Celery()
 app.conf.update(settings.CELERY)
