be1182578ed27519df49019d63bd8af7651f4eeb
bildad namawa <bildadnamawa@gmail.com>
2019-10-30 23:47:30 +0300

    auto-commit on change (2019-10-30 23:47:30) by piladi-bot

--- face_detect_api/settings.py
+++ face_detect_api/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'minio_storage',
     'api',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- face_detect_api/urls.py
+++ face_detect_api/urls.py
@@ -16,8 +16,28 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# swagger api view
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
