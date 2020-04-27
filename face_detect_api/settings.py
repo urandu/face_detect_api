e93e969a38418e824e60f75b2fc8c3571483a607
bildad namawa <bildadnamawa@gmail.com>
2020-04-27 08:42:48 +0300

    auto-commit on change (2020-04-27 08:42:48) by piladi-bot

--- face_detect_api/settings.py
+++ face_detect_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+from socket import gethostname, gethostbyname 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
