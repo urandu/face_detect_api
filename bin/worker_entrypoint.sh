c68f8247799e6e70d45f445f02962ed59e5bf2d9
bildad namawa <bildadnamawa@gmail.com>
2020-04-27 07:26:57 +0300

    auto-commit on change (2020-04-27 07:26:57) by piladi-bot

--- bin/worker_entrypoint.sh
+++ bin/worker_entrypoint.sh
@@ -3,4 +3,5 @@
 set -e
 
 echo "Starting worker container..."
-celery worker --app=apps.api.celery_app --concurrency=3 --scheduler django_celery_beat.schedulers:DatabaseScheduler --hostname=worker@%h --loglevel=INFO
+
+celery worker --app=api.celery_app --concurrency=2 --hostname=worker@%h --loglevel=INFO
