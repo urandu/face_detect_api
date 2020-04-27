2b0cbd77ad83f4f7709a9e504017c94d96096002
bildad namawa <bildadnamawa@gmail.com>
2020-04-27 07:35:24 +0300

    auto-commit on change (2020-04-27 07:35:24) by piladi-bot

--- bin/minio_entrypoint.sh
+++ bin/minio_entrypoint.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-echo "Making migrations ..."
-python manage.py makemigrations
+echo "Starting minio ..."
+server /data
 
-echo "Collect static files ..."
-#python manage.py collectstatic
-
-echo "Applying migrations ..."
-python manage.py migrate
-
-echo "Starting web container..."
-python manage.py runserver 0.0.0.0:8000
